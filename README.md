e1345658c1c7fe7103620c2529cef235f6087eb6
nmtiwari <nmtiwari@iastate.edu>
2015-09-06 16:41:19 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Candoia app: Wait-Notify Police
+
+This app computes and visualizes number of violations of wait-notify idioms.
+
+in the project. To run add this app to your Candoia installation.
+
+
+## Structure of this directory
+
+
+- main.html: describes visual layout of the app pane in the platform
+
+- main.js: contains glue code for interaction
+
+- app.css: app's stylesheet
+
+- wait-notify-police.boa: contains MSR logic 
+
+- package.json: metadata about the app
+
+- lib: libraries used by this app
---
+++ main.js
@@ -0,0 +1,52 @@
+'use strict';
+$(window).load(function() {
+  let scm = new ColorScheme;
+  scm.from_hue(21).scheme('triade').distance(0.1).add_complement(false).variation('pastel').web_safe(true);
+  let colors = scm.colors();
+  let instance = api.instance.get();
+
+  let json = api.boa.run('wait-notify-police.boa');
+  $('#loading').hide();
+  $('#content').show();
+
+  let canvas = $('#chart-output').get(0).getContext("2d");
+  let chartData = [];
+  let count = 0;
+
+  for(let index in json.out) {
+    count++;
+    $('#numToShow').append(`<option value="${count}"> ${count} </option>`);
+    chartData.push({
+        label: index,
+        value: json.out[index],
+        color: '#' + _.sample(colors)
+    });
+  }
+
+  $('#numToShow').change(function() {
+    $('#output').html('<canvas id="chart-output"> </canvas>');
+    canvas = $('#chart-output').get(0).getContext('2d');
+    display($('#numToShow').val());
+  });
+
+  chartData = _.sortBy(chartData, function(line) {
+    return Number(-line.value);
+  });
+
+  display(1);
+
+  $('#app-title').html(`Number of wait-notify idiom violations in ${instance.repos.name}`);
+
+  function display(num) {
+    let limitedData = _.first(chartData, num);
+
+    $('#table-output-body').html('');
+    _.each(limitedData, function(element, index, list) {
+      let num = index + 1;
+      $('#table-output-body').append(`<tr><td> ${num} </td> <td> ${element.label} </td> <td> ${element.value} </td> </tr>`)
+    });
+
+    let outputChart = new Chart(canvas).Pie(limitedData, {responsive: true});
+  }
+
+});
